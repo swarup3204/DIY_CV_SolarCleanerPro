d23450348397222544dce3c9f3c35ccdbf8ede1b
Swarup Padhi <swarupksms@gmail.com>
2022-06-28 18:48:27 +0530

    Update lineDetect.py

--- lineDetect.py
+++ lineDetect.py
@@ -8,23 +8,30 @@ def getLinedImg(img):
     lines = cv2.HoughLines(edges,1,np.pi/180, 200)
     line_length = 2000
     try:
+        lst = []
         for i in range(len(lines)):
             for r_theta in lines[i]:
+                if(len(lst)>=2):
+                    break
                 r,theta = r_theta
-                a = np.cos(theta)
-                b = np.sin(theta)
-                x0 = a*r
-                y0 = b*r
-                x1 = int(x0 + line_length*(-b))
-                y1 = int(y0 + line_length*(a))
-                x2 = int(x0 - line_length*(-b))
-                y2 = int(y0 - line_length*(a))
-                cv2.line(img,(x1,y1), (x2,y2), (0,0,255),2)
+                # print(theta)
+                if theta > 1.4 and theta < 1.8: 
+                    a = np.cos(theta)
+                    b = np.sin(theta)
+                    x0 = a*r
+                    y0 = b*r
+                    x1 = int(x0 + line_length*(-b))
+                    y1 = int(y0 + line_length*(a))
+                    x2 = int(x0 - line_length*(-b))
+                    y2 = int(y0 - line_length*(a))
+                    cv2.line(img,(x1,y1), (x1,y2), (0,0,255),2)
+                    lst.append(y1)
+        print(lst)        
     except:
         pass
     return img
 
 if __name__ == '__main__':
-    img = cv2.imread('image.jpg')
+    img = cv2.imread('img5.jpg')
     new_img = getLinedImg(img)
     cv2.imwrite('linesDetected.jpg', new_img)
