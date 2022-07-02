ac794f576baf1ffd50a9ce20bdc00ceb16a5d465
grce47 <gracesharma47@gmail.com>
2022-07-02 17:42:33 +0530

    update live.py

--- liveLineDetection.py
+++ liveLineDetection.py
@@ -1,15 +1,22 @@
 from lineDetect import getLinedImg
 import cv2
 
-vid = cv2.VideoCapture(0)
+if __name__ == "__main__":
+    
+    vid = cv2.VideoCapture(0)
 
-while(True):
-    ret, frame = vid.read()
-    lineFrame, close_edge = getLinedImg(frame)
-    cv2.imshow('frame', lineFrame)
-    print(close_edge)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    NO_EGDE_FOUND = "NO_EGDE_FOUND"
+    NOT_CLOSE = "NOT_CLOSE"
+    CLOSE = "CLOSE"
 
-vid.release()
-cv2.destroyAllWindows()
+    while(True):
+        ret, frame = vid.read()
+        lineFrame, close_edge = getLinedImg(frame)
+        cv2.imshow('frame', lineFrame)
+        if close_edge != NO_EGDE_FOUND:
+            print(close_edge)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    vid.release()
+    cv2.destroyAllWindows()
